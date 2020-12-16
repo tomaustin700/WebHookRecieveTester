dc19e6979b58cafcc92bd34f42fddd4ccad9ce86
Tom Austin <Tom.Austin@streets-heaver.com>
2020-12-16 13:18:41 +0000

    Add project files.

---
+++ WebHookRecieveTester/Controllers/RecieveController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHookRecieveTester.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecieveController : ControllerBase
+    {
+       
+        private readonly ILogger<RecieveController> _logger;
+
+        public RecieveController(ILogger<RecieveController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task Recieve()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = await reader.ReadToEndAsync();
+
+                _logger.LogInformation(body);
+            }
+        }
+    }
+}
